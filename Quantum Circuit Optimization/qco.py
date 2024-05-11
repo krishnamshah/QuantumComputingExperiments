27d3c94bb6c7adf75c0de0fa0475370343f67791
Krishna Mohan Shah <mailforkrishnashah@gmail.com>
2024-05-11 13:34:53 +0200

    Optimized QCO

--- Quantum Circuit Optimization/qco.py
+++ Quantum Circuit Optimization/qco.py
@@ -1,22 +1,32 @@
 from qiskit import QuantumCircuit, transpile
 from qiskit.providers.aer import AerSimulator
+from qiskit.circuit.random import random_circuit
+import random
 
-# Create a quantum circuit with redundant operations
-qc = QuantumCircuit(1)
 
-# Redundant operations
-qc.h(0)
-qc.h(0)
-qc.x(0)
-qc.x(0)
+def optimize_circuit(qc):
+    # Optimize the circuit using Qiskit's transpiler with optimization level 3
+    optimized_qc = transpile(qc, AerSimulator(), optimization_level=3)
+    return optimized_qc
 
-# Print the original circuit
-print("Original Circuit:")
-print(qc.draw())
 
-# Optimize the circuit using Qiskit's transpiler with optimization level 3
-optimized_qc = transpile(qc, AerSimulator(), optimization_level=3)
+def generate_and_optimize_random_circuit(n_qubits, depth):
+    # Generate a random quantum circuit
+    qc = random_circuit(n_qubits, depth, measure=True)
 
-# Print the optimized circuit
-print("\nOptimized Circuit:")
-print(optimized_qc.draw())
+    # Print the original circuit
+    print("Original Circuit:")
+    print(qc.draw())
+
+    # Optimize the circuit
+    optimized_qc = optimize_circuit(qc)
+
+    # Print the optimized circuit
+    print("\nOptimized Circuit:")
+    print(optimized_qc.draw())
+
+
+# Generate and optimize a random circuit
+n_qubits = random.randint(1, 5)  # Random number of qubits between 1 and 5
+depth = random.randint(1, 5)  # Random depth between 1 and 5
+generate_and_optimize_random_circuit(n_qubits, depth)
